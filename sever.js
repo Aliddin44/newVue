3876feac730a40b2b9a4c5a01128c04e762c0f98
asliddin4466 <asliddin4466@gmail.com>
2022-10-23 22:37:58 +0500

    assalom

--- sever.js
+++ sever.js
@@ -5,11 +5,13 @@ const path = require('path')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
+app.get(/.*/, function(req,res){
+    res.sendFile(path.join(__dirname,'/dist/index.html'))
+})
 const port = process.env.PORT || 5000
 app.listen(port)
 
-console.log('listening on port: ' +port)
+console.log(`listening on port: ' ${port}`)
 
 
 
