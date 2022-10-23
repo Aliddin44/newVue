07956697ab8008d6c7aa957cb2f0510fb087ccd9
asliddin4466 <asliddin4466@gmail.com>
2022-10-23 22:58:42 +0500

    salom

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-
+  
 ]
 
 const router = createRouter({
